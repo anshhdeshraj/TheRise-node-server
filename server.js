91cf743fe08aa908c49c1676fc375362a6fab61f
Ansh Deshraj <126981356+anshhdeshraj@users.noreply.github.com>
2024-04-12 16:28:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 80;
 const router = require('./router');
 const mongodbCredentials = {
   DB_username : 'ansh1515',
